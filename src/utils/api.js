789246496fd219102281b7097297b254cbc8b4e8
Achal Vitthal Sawant <146034830+SawantAchal@users.noreply.github.com>
2024-03-17 13:53:08 +0530

    Update API key api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,8 +1,9 @@
+API_KEY=eb29ffaea9dd4af49d98e47ec17ec3f4
 
 // Function to fetch current weather data for a specified city
   export const getWeatherData = async (city) => {
     try {
-      const url =` https://api.weatherbit.io/v2.0/current?city=${encodeURIComponent(city)}&key=fdae205ac152404fa1cd9cd19ce086ea`;
+      const url =` https://api.weatherbit.io/v2.0/current?city=${encodeURIComponent(city)}&key=${API_KEY}`;
       const response = await fetch(url);
       if (!response.ok) {
         throw new Error('Failed to fetch weather data');
@@ -23,7 +24,7 @@
   // Function to fetch weather forecast data for a specified city
   export const getWeatherForecast = async (city) => {
     try {
-      const response = await fetch(`https://api.weatherbit.io/v2.0/forecast/daily?city=${encodeURIComponent(city)}&days=7&key=fdae205ac152404fa1cd9cd19ce086ea`);
+      const response = await fetch(`https://api.weatherbit.io/v2.0/forecast/daily?city=${encodeURIComponent(city)}&days=7&key=${API_KEY}`);
      
       if (!response.ok) {
         throw new Error('Failed to fetch weather forecast data');
